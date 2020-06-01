b6f249254cbca8c5a2a1bfd11e8a54970f2bb62d
fellyph <fellyph.cintra@gmail.com>
2020-06-01 14:01:49 +0100

    adding comments and final exercise

--- 03_indexedDB/js/02-example-indexedDB-defining-schema-final.js
+++ 03_indexedDB/js/02-example-indexedDB-defining-schema-final.js
@@ -1,14 +1,32 @@
 /*
-    O que e indexedDB
+  # O que e indexedDB
   - banco de dados nao relacional
   - permite armazenamento de objetos no browser do usuário
   - nos permite armazenar objetos javascript, arquivos, blobs
   - suporta transacoes
   - la podemos definir multiplos indices para realizar consultas
 
-  Bibliotecas que facilitam o uso de indexedDB
+  ## Bibliotecas que facilitam o uso de indexedDB
   - IndexedDB Promised
   - localForage
+
+  ### Banco de dados
+
+  ### Object Store
+  - Possui o comportamento similar ao comportamento de tabelas.
+  - O armazena objetos organizados por tipos, não são os tipos padrões javaScript mas tipos que definimos durante a construcão de nosso banco
+
+  ### Index
+  - O indice é um tipo criado para organizar nossa Object Store, por uma propriedade individual
+  - Um timpo de informação que podemos armazenar para consulta.
+
+  ### Transactions
+  - Um transação é um pacote que um grupo de operações para manter a integridade do banco de dados
+  - Se uma operação dentro de uma transação falhar, toda a transação será cancelada
+  - Todas operações de leitura e escrita precisam fazer parte de uma transação
+
+  ### Cursor
+  - Um mecanismo interar sobre os registros do banco de dados.
 
   // - https://www.youtube.com/watch?v=vb7fkBeblcw
 */
@@ -45,7 +63,11 @@ const createDB = () => {
           keyPath: 'id',
           autoIncrement: true
         });
+
+      // creating a index it can repeat if we pass the value unique as false
       objectStore.createIndex('lat', 'lat', {unique: false});
+
+      // creating a second index the objectStore can have more than one
       objectStore.createIndex('log', 'log', {unique: false});
 
       console.log('Config completed');
